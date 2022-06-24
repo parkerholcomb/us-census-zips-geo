a321c26f2beee9db551abf1551138c2962c9a9dd
Parker Holcomb <parker.holcomb@anthem.com>
2022-06-24 10:43:31 -0500

    us adi summary

--- adi/lib.py
+++ adi/lib.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import plotly.express as px
 
+px.set_mapbox_access_token(open(".mapbox_token").read())
+
 ### 
 
 def get_state_codes() -> list:
@@ -8,7 +10,7 @@ def get_state_codes() -> list:
 
 def us_adi_zip5_stats(us_adi: pd.DataFrame) -> pd.DataFrame:
     us_zips = pd.read_feather("data/us_zips.feather")
-    df = us_adi.groupby(['_zip5']).agg(
+    df = us_adi.groupby(['_state','_zip5']).agg(
         count=("_adi","count"),
         min=("_adi","min"),
         max=("_adi","max"),
@@ -17,13 +19,32 @@ def us_adi_zip5_stats(us_adi: pd.DataFrame) -> pd.DataFrame:
     ).reset_index()
     return df.merge(us_zips).dropna()
 
+def draw_us_adi_distribution(us_adi):
+    adi_freq = us_adi.groupby(['_adi']).agg(count=("_adi","count")).reset_index()
+    adi_freq['_pct'] = adi_freq['count'].apply(lambda x: x/adi_freq['count'].sum())
+    px.bar(
+        adi_freq, x='_adi', y="_pct", height=300, width=600,
+        title="US ADI Distribution"
+    ).show(renderer='notebook')
+
+def draw_adi_map(adi_stats: pd.DataFrame, state_codes = []) -> None:
+    if len(state_codes) > 0:
+        df = adi_stats[adi_stats['_state'].isin(state_codes)]
+    else:
+        df = adi_stats
+    px.scatter_mapbox(
+        df, lat="_lat", lon="_lng", size_max=15, 
+        height=600, zoom=3, color='adi_mean', size='_census_total',
+        title=f"ADI Mean | centroids = _zip5 | scaled by census total"
+    ).show(renderer='notebook')
+
 
 ## once extracted
 
 def _features_only(df): return df[[name for name in df.columns if name.startswith("_") ]]
 
 def _load_adi_by_state(state = "TX", features_only=True):
-    df = pd.read_parquet(f"data/tables/{state}.parquet")
+    df = pd.read_parquet(f"data/state_tables/{state}.parquet")
     df = df[df['TYPE'].isna()] # this is only the "standard zip codes"
     df['_zip3'] = df['ZIPID'].apply(lambda x: x[1:4])
     df['_zip5'] = df['ZIPID'].apply(lambda x: x[1:6])
